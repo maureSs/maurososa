950b11ffd36d1122c475c949acea2e12b92556ab
maureSs <mauroaf4@gmail.com>
2024-01-29 19:18:23 -0300

    tailwind content route fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.{js,ts,jsx,tsx,mdx}'
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {}
