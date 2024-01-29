4ba1e6d9d0840c306df5e71acb5706c97f3b46ea
maureSs <mauroaf4@gmail.com>
2024-01-29 19:40:48 -0300

    trying to fix mobile on vercel

--- src/app/components/NavMenu.tsx
+++ src/app/components/NavMenu.tsx
@@ -25,15 +25,12 @@ function NavMenu({ colorMode }: { colorMode: string }) {
 
   return (
    <div style={pathname === '/' ? {display: 'none'} : {display:'flex'}} 
-        className={`flex flex-col nav-mobile-layout 
-        w-${windowWidth > 480 ? '30' : '80'}vh h-${windowWidth > 480 ? '90' : '25'}vh 
-        ${colorMode === 'dark' ? 'text-white' : 'text-black'}
+        className={`flex flex-col nav-mobile-layout ${colorMode === 'dark' ? 'text-white' : 'text-black'}
         self-start p-8 pt-12`}
     >
     <h2 className="text-4xl mb-4 font-bold">Mauro Sosa</h2>
     <p className="mb-4 ml-1">Web Developer</p>
-    <div className={`flex ${windowWidth > 480 ? 'flex-col' : 'flex-row'} 
-      md:mt-8 lg:mt-12 text-lg bg-slate-100 md:bg-transparent
+    <div className={`flex flex-row md:flex-col md:mt-8 lg:mt-12 text-lg bg-slate-100 md:bg-transparent
       ${windowWidth < 480 && 'fixed top-5 left-10 border-2 rounded-full' } 
       `}>
       <Link href="/about" className='nav-anchor'>About</Link>
