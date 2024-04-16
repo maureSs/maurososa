9aef522c74a04885d7a6abbf9e8a92ae9ce56243
maureSs <mauroaf4@gmail.com>
2024-04-16 20:32:21 -0300

    Pope Wall added to projects

--- src/app/api.ts
+++ src/app/api.ts
@@ -9,88 +9,88 @@ import node from '../../public/assets/images/node.png'
 import tailwind from '../../public/assets/images/tailwind.png'
 import express from '../../public/assets/images/express.png'
 // projects
-import kingpudding from '../../public/assets/images/kingpudding.jpg'
+import popewall from '../../public/assets/images/popewall.jpg'
 import mustWatch from '../../public/assets/images/mustWatch.jpg'
 import gamingStore from '../../public/assets/images/gamingStore.jpg'
 import quizAboutEverything from '../../public/assets/images/quizAboutEverything.jpg'
 
 export const aboutMe = '25 y/o web developer from Buenos Aires, Argentina. I mainly work with TypeScript, React and Next.js, and but I love learning new technologies and tools.'
- 
+
 export const myTechnologies = [
-    {
-       name: "TypeScript",
-       url: "https://www.typescriptlang.org/",
-       image: ts
-    },
-    {
-        name: "JavaScript",
-        url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
-        image: js
-     },
-     {
-        name: "Next.js",
-        url: "https://nextjs.org/",
-        image: next
-     },
-     {
-        name: "React.js",
-        url: "https://react.dev/",
-        image: react
-     },
-     {
-        name: "Redux.js",
-        url: "https://redux.js.org/",
-        image: redux
-     },
-     {
-        name: "Node.js",
-        url: "https://nodejs.org/en/",
-        image: node
-     },
-     {
+   {
+      name: "TypeScript",
+      url: "https://www.typescriptlang.org/",
+      image: ts
+   },
+   {
+      name: "JavaScript",
+      url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+      image: js
+   },
+   {
+      name: "Next.js",
+      url: "https://nextjs.org/",
+      image: next
+   },
+   {
+      name: "React.js",
+      url: "https://react.dev/",
+      image: react
+   },
+   {
+      name: "Redux.js",
+      url: "https://redux.js.org/",
+      image: redux
+   },
+   {
+      name: "Node.js",
+      url: "https://nodejs.org/en/",
+      image: node
+   },
+   {
       name: "Express.js",
-      url: "https://expressjs.com/", 
+      url: "https://expressjs.com/",
       image: express
-     },
-     {
+   },
+   {
       name: "MongoDB",
       url: "https://www.mongodb.com/",
       image: mongodb
    },
-     {
-        name: "Tailwind CSS",
-        url: "https://tailwindcss.com/resources",
-        image: tailwind
-     }
-  ]
-
-  export const myProjects = [
    {
-      name: "kingpudding",
-      image: kingpudding,
-      description: "Pastry Shop local entrepreneurship",
-      technologies: ['JavaScript', 'React.js'],
-      url: "https://kingpudding.netlify.app/"
+      name: "Tailwind CSS",
+      url: "https://tailwindcss.com/resources",
+      image: tailwind
+   }
+]
+
+export const myProjects = [
+   {
+      name: "Pope Wall",
+      image: popewall,
+      description: "Website about security",
+      technologies: ['TypeScript', 'React.js', 'Next.js', 'Tailwind CSS'],
+      url: "https://pope-wall.vercel.app/"
    },
    {
       name: "must-watch",
       image: mustWatch,
       description: "Use must-watch to take note of pending-to-watch movies and tv shows",
-      technologies: ['JavaScript','React.js', 'Firebase'],
+      technologies: ['JavaScript', 'React.js', 'Firebase'],
       url: "https://must-watch-81fcf.web.app/login"
-    },
-    {
+   },
+   {
       name: "gaming-store",
       image: gamingStore,
       description: "An ecommerce simulator for purchasing gaming products",
-      technologies: ['TypeScript','React.js'],
+      technologies: ['TypeScript', 'React.js'],
       url: "https://gaming-store.netlify.app/"
-    },
-    {
+   },
+   {
       name: "quiz about everything",
       image: quizAboutEverything,
       description: "A fun Trivia-Quiz game!",
-      technologies: ['TypeScript','React.js'],
+      technologies: ['TypeScript', 'React.js'],
       url: "https://quiz-about-everything.netlify.app/"
-    }
-  ]
+   }
+]
